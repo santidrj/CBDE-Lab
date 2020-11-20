546f7ad9ea2358f35e2a8484a2791095a603508c
santidrj <santi.delrey@gmail.com>
2020-11-20 20:39:08 +0100

    Finish HBase exercise 2

--- HBaseEntrega/src/main/java/cbde/labs/hbase_mapreduce/reader/MyHBaseReader_KeyDesign.java
+++ HBaseEntrega/src/main/java/cbde/labs/hbase_mapreduce/reader/MyHBaseReader_KeyDesign.java
@@ -2,12 +2,12 @@ package cbde.labs.hbase_mapreduce.reader;
 
 public class MyHBaseReader_KeyDesign extends MyHBaseReader {
 
-	protected String scanStart() {
-		return null;
-	}
-	
-	protected String scanStop() {
-		return null;
-	}
-		
+    protected String scanStart() {
+        return "A";
+    }
+
+    protected String scanStop() {
+        return "B";
+    }
+
 }
--- HBaseEntrega/src/main/java/cbde/labs/hbase_mapreduce/writer/MyHBaseWriter_KeyDesign.java
+++ HBaseEntrega/src/main/java/cbde/labs/hbase_mapreduce/writer/MyHBaseWriter_KeyDesign.java
@@ -2,8 +2,13 @@ package cbde.labs.hbase_mapreduce.writer;
 
 public class MyHBaseWriter_KeyDesign extends MyHBaseWriter {
 
-	protected String nextKey() {
-		return null;
-	}
-		
+    protected String nextKey() {
+        String type = data.get("type");
+        String region = data.get("region");
+        if ("type_1".equals(type) && "3".equals(region)) {
+            return "A" + key;
+        }
+        return "Z" + key;
+    }
+
 }
