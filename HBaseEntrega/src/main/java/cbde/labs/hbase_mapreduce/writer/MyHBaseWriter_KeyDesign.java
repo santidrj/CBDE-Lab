fb061d0946d9df82285dc78af4c67b373f8f095c
santidrj <santi.delrey@gmail.com>
2020-11-20 20:59:02 +0100

    Create MapReduceEntrega

--- HBaseEntrega/src/main/java/cbde/labs/hbase_mapreduce/writer/MyHBaseWriter_KeyDesign.java
+++ HBaseEntrega/src/main/java/cbde/labs/hbase_mapreduce/writer/MyHBaseWriter_KeyDesign.java
@@ -5,7 +5,7 @@ public class MyHBaseWriter_KeyDesign extends MyHBaseWriter {
     protected String nextKey() {
         String type = data.get("type");
         String region = data.get("region");
-        if ("type_1".equals(type) && "3".equals(region)) {
+        if ("type_3".equals(type) && "0".equals(region)) {
             return "A" + key;
         }
         return "Z" + key;
